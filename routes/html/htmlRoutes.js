9ff54d57a7ac75afbff0edff4f740c5251c59a90
Sodaman <mattrick2214@gmail.com>
2022-03-11 01:58:14 -0600

    Got it to load to locahost with my error message.

--- routes/html/htmlRoutes.js
+++ routes/html/htmlRoutes.js
@@ -1,4 +1,8 @@
 const router = require('express').Router();
 const path = require('path');
 
+router.get('/user', (req, res) => {
+    res.sendFile(path.join(__dirname, '../../public/user.html'));
+});
+
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 
 const apiRoutes = require('./api');
-const htmlRoutes = require('./html/html-routes');
+const htmlRoutes = require('./html/htmlRoutes');
 
 router.use('/api', apiRoutes);
 router.use('/', htmlRoutes);
