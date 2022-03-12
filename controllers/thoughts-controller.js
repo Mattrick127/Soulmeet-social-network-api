4448061fe507c96933276eafcb819e0d303c2115
Sodaman <mattrick2214@gmail.com>
2022-03-11 19:50:41 -0600

    Fixed image of Twin Humanities for Soulmeet and worked on controllers.

--- controllers/thoughts-controller.js
+++ controllers/thoughts-controller.js
@@ -1,7 +1,15 @@
 const { Thought, User } = require('../models');
 
 const thoughtsController = {
-    
+    addThought({ params, body }, res) {
+        console.log(params);
+        Thought.create(body)
+        .then(({ _id }) => {
+            return Thought.findOneAndUpdate(
+                
+            )
+        })
+    }
 }
 
 module.exports = thoughtsController;
