156e785bd280e664ba9d43a83902f274dac0f155
Sodaman <mattrick2214@gmail.com>
2022-03-11 19:39:12 -0600

    Finished writing in models!

--- models/Thought.js
+++ models/Thought.js
@@ -1,6 +1,34 @@
 const { Schema, model, Types } = require('mongoose');
 const formatDate = require('../utils/formatDate');
 
+const ReactionSchema = new Schema(
+    {
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId()
+        },
+        reactionBody: {
+            type: String,
+            required: true,
+            maxlength: 280
+        },
+        username: {
+            type: String,
+            required: true
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: createdAtVal => formatDate(createdAtVal)
+        }
+    },
+    {
+        toJSON: {
+            getters: true
+        }
+    }
+)
+
 const ThoughtSchema = new Schema(
     {
         thoughtText: {
@@ -18,7 +46,7 @@ const ThoughtSchema = new Schema(
             type: String,
             required: true
         },
-        reactions: [reactionSchema]
+        reactions: [ReactionSchema]
         },
     {
         toJSON: {
@@ -28,13 +56,6 @@ const ThoughtSchema = new Schema(
         id: false
     }
 );
-
-const reactionSchema = new Schema(
-    {
-        
-    }
-)
-
 
 ThoughtSchema.virtual('reactionCount').get(function() {
     return this.reactions.length;
