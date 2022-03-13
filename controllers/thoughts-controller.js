c42e4546163956e186b01ccaf9d2f8b92a5aff3e
Sodaman <mattrick2214@gmail.com>
2022-03-12 19:31:13 -0600

    changed all version of 'thought' to 'thoughts' to avoid further confusion.

--- controllers/thoughts-controller.js
+++ controllers/thoughts-controller.js
@@ -1,15 +1,40 @@
-const { Thought, User } = require('../models');
+const { Thoughts, User } = require('../models');
 
 const thoughtsController = {
     addThought({ params, body }, res) {
         console.log(params);
-        Thought.create(body)
+        Thoughts.create(body)
         .then(({ _id }) => {
-            return Thought.findOneAndUpdate(
+            return Thoughts.findOneAndUpdate(
                 { _id: params.userId },
-                { $push: { }}
-            )
+                { $push: { thought: _id } },
+                { new: true }
+            );
         })
+        .then(dbUserData => {
+            console.log(dbUserData);
+            if(!dbUserData) {
+                res.status(404).json({ message: 'No Thoughts found with this id!' });
+                return;
+            }
+            res.json(dbUserData);
+        })
+        .catch(err => res.json(err));
+    },
+    getAllThoughts(req, res) {
+        Thoughts.find({})
+        .populate({
+            path: 'thoughts'
+        })
+    },
+
+
+    addReaction({ params, body }, res) {
+        Reaction.findOneAndUpdate(
+            { _id: params.thoughtsId },
+            { $push: { replies: body } },
+            { new: true, runValidators: true }
+        )
     }
 }
 
